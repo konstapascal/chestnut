df8882b656b57467ef4171e895b9341c110883c3
Konstantinos Pascal <pascalkonstantinos@gmail.com>
2020-03-27 06:51:41 +0100

    Cleaned up file structure in components

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Navbar from './components/navbar.js';
+import App from './components/app.js';
 
 const rootElement = document.getElementById('root');
 
-ReactDOM.render(<Navbar />, rootElement);
+ReactDOM.render(<App />, rootElement);
